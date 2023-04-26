f3d843744e45ca51826293a7f6f8508a61a922b0
shinrinakamura <33050174+shinrinakamura@users.noreply.github.com>
2023-04-26 21:23:06 +0900

    クラウド環境作成のためのスクリプトです

---
+++ sipf_sample_cloud.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# install node-red
+sudo apt update
+bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
+sudo systemctl start nodered.service
+sudo systemctl enable nodered.service
+
+# install influxdb
+sudo curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
+sudo echo "deb https://repos.influxdata.com/ubuntu bionic stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
+sudo echo "deb https://repos.influxdata.com/ubuntu bionic stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
+sudo apt update
+sudo apt install -y influxdb
+sudo systemctl start influxdb
+sudo apt install -y influxdb-client
+
+# install grafana
+sudo apt-get install -y apt-transport-https
+sudo apt-get install -y software-properties-common wget
+sudo wget -q -O /usr/share/keyrings/grafana.key https://apt.grafana.com/gpg.key
+echo "deb [signed-by=/usr/share/keyrings/grafana.key] https://apt.grafana.com stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+echo "deb [signed-by=/usr/share/keyrings/grafana.key] https://apt.grafana.com beta main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+sudo apt update
+sudo apt install grafana
+
+sudo systemctl daemon-reload
+sudo systemctl start grafana-server
