d5a0bfb617dce64ac69f3eb31aee292f13b9bbd7
shinrinakamura <33050174+shinrinakamura@users.noreply.github.com>
2023-04-27 20:53:41 +0900

    grafanaのインストールの-yオプションをつけた

--- sipf_sample_cloud.sh
+++ sipf_sample_cloud.sh
@@ -22,7 +22,7 @@ sudo wget -q -O /usr/share/keyrings/grafana.key https://apt.grafana.com/gpg.key
 echo "deb [signed-by=/usr/share/keyrings/grafana.key] https://apt.grafana.com stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
 echo "deb [signed-by=/usr/share/keyrings/grafana.key] https://apt.grafana.com beta main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
 sudo apt update
-sudo apt install grafana
+sudo apt -y install grafana
 
 sudo systemctl daemon-reload
 sudo systemctl start grafana-server
